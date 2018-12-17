dc65c8f30efb1426bb0f3089e422d35fc97785b6
keith <hybridwebtech@gmail.com>
2018-12-17 06:12:49 -0700

    Added disease classifier libraries

---
+++ src/app/components/classifiers/BPClassifier.js
@@ -0,0 +1,43 @@
+let testBPData = [ {SysBP: 120, DiaBP: 90, atDate: '2018/10/31'},{SysBP: 115, DiaBP: 100, atDate: '2018/10/20'}];
+
+function hypertensionCalculator( bpData )
+{
+    if (!Array.isArray(bpData))
+    {
+        return "Input data is not an array. Cannot compute BP classification.";
+    }
+
+    if (bpData.length === 0)
+    {
+        return "Input array does not contain any data. Cannot compute BP classification";
+    }
+
+    let sortedArray = bpData.sort( (a, b) => {
+        let ad = new Date(a.atDate);
+        let bd = new Date(b.atDate);
+        return bd.getTime() - ad.getTime();
+    } );
+
+    let latestBP = sortedArray[0];
+
+    let classification = "No Hypertension";
+
+    if ( latestBP.SysBP >= 180 && latestBP.DiaBP >= 120 )
+    {
+        classification = "Stage 3";
+    }
+    else if ( (latestBP.SysBP >= 160 && latestBP.SysBP < 180) ||
+              (latestBP.DiaBP >= 100 && latestBP.DiaBP < 100) )
+    {
+        classification = "Stage 2";
+    }
+    else if ( (latestBP.SysBP >= 140 && latestBP.SysBP < 160) ||
+              (latestBP.DiaBP >= 90 && latestBP.DiaBP < 100) )
+    {
+        classification = "Stage 1";
+    }
+
+    return "Latest BP at " + latestBP.atDate + " Systolic:" + latestBP.SysBP + "; Diastolic: " + latestBP.DiaBP + "; Classification: " + classification ;
+}
+
+console.log( hypertensionCalculator( testBPData ) );
---
+++ src/app/components/classifiers/KidneyDiseaseClassifier.js
@@ -0,0 +1,82 @@
+let testKidneyData = [{eGFR: 65, atDate: '2018/10/31'}, {eGFR: 70, atDate: '2018/10/20'}];
+
+function kidneyFunctionCalculator( kidneyData )
+{
+    if (!Array.isArray(kidneyData))
+    {
+        return "Input data is not an array. Cannot compute GFR classification.";
+    }
+
+    if (kidneyData.length === 0)
+    {
+        return "Input array does not contain any data. Cannot compute GFR classification";
+    }
+
+    let sortedArray = kidneyData.sort( (a, b) => {
+        let ad = new Date(a.atDate);
+        let bd = new Date(b.atDate);
+        return bd.getTime() - ad.getTime();
+    } );
+
+    return calculateLatestStatus( sortedArray )
+        .join( checkForConsecutiveGFRDrops( sortedArray ) );
+}
+
+function calculateLatestStatus( sortedArray )
+{
+    var latestGFR = sortedArray[0];
+
+    var latestClassification = "Kidney Failure";
+    if ( latestGFR.eGFR >= 90 )
+    {
+        latestClassification = "Normal";
+    }
+    else if ( latestGFR.eGFR >= 60 && latestGFR.eGFR <= 89 )
+    {
+        latestClassification = "Mildly Decreased";
+    }
+    else if ( latestGFR.eGFR >= 45 && latestGFR.eGFR <= 59 )
+    {
+        latestClassification = "Mild to Moderate";
+    }
+    else if ( latestGFR.eGFR >= 30 && latestGFR.eGFR <= 44 )
+    {
+        latestClassification = "Moderate to Severe";
+    }
+    else if ( latestGFR.eGFR >= 15 && latestGFR.eGFR <= 29 )
+    {
+        latestClassification = "Severely Decreased";
+    }
+
+    let latestStatus = "Latest eGFR at " + latestGFR.atDate + " eGFR: " + latestGFR.eGFR + "; Classification: " + latestClassification;
+
+    return [latestStatus];
+}
+
+function checkForConsecutiveGFRDrops( sortedArray )
+{
+    let statusArray = new Array();
+
+    if ( sortedArray.length > 0 )
+    {
+        for ( var i = 1; i < sortedArray.length; i++ )
+        {
+            let prevReading = sortedArray[i - 1];
+            let currReading = sortedArray[i];
+
+            var deltaGFR = currReading.eGFR - prevReading.eGFR;
+
+            var percentChange = (deltaGFR/sortedArray[i].eGFR)*100.0;
+
+            if ( percentChange >= 20.0 )
+            {
+                statusArray.append( "Consecutive eGFR drop: " + percentChange + ", between " + prevReading.atDate + "(" + prevReading.eGFR +
+                    ") and " + currReading.atDate + "(" + currReading.eGFR + ")" );
+            }
+        }
+    }
+
+    return statusArray;
+}
+
+console.log( kidneyFunctionCalculator( testKidneyData ) );
